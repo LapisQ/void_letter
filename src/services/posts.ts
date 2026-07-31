e186069db765ce0d66e47d55cc908a41430f11d8
Prince_Afdob <anonymouscognato@gmail.com>
2026-07-31 17:38:43 +0530

    Update posts.ts

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,23 +1,23 @@
-import { apiRequest } from "./api";
+// import { apiRequest } from "./api";
 
-export async function getPosts() {
-  return apiRequest("getPosts", "GET");
-}
+// export async function getPosts() {
+//   return apiRequest("getPosts", "GET");
+// }
 
-export async function createPost(post: unknown) {
-  return apiRequest("addPost", "POST", post);
-}
+// export async function createPost(post: unknown) {
+//   return apiRequest("addPost", "POST", post);
+// }
 
-export async function getLatestPosts() {
-  return apiRequest("latest", "GET");
-}
+// export async function getLatestPosts() {
+//   return apiRequest("latest", "GET");
+// }
 
-export async function getRandomPost() {
-  return apiRequest("random", "GET");
-}
+// export async function getRandomPost() {
+//   return apiRequest("random", "GET");
+// }
 
-export async function likePost(id: number) {
-  return apiRequest("like", "POST", {
-    id,
-  });
-}
+// export async function likePost(id: number) {
+//   return apiRequest("like", "POST", {
+//     id,
+//   });
+// }
